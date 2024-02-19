79324fdcd9657bf93486b684ca408a8d0da5ff8e
Philip Mercan <philmercan@gmail.com>
2024-02-19 11:37:31 +0100

    adding log4j2 dependency and set up jacoco full report

--- src/test/java/net/safety/alerts/integration/FireStationServiceWithDaoIT.java
+++ src/test/java/net/safety/alerts/integration/FireStationServiceWithDaoIT.java
@@ -1,17 +1,21 @@
-package net.safety.alerts.service;
+package net.safety.alerts.integration;
 
+import net.safety.alerts.service.FireStationService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-public class FireStationServiceIT {
+@ExtendWith(SpringExtension.class)
+public class FireStationServiceWithDaoIT {
 
 
     @Autowired
