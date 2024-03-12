e3aa368c46d5c5978f51ce12f37b96b7f804bee6
Philip Mercan <philmercan@gmail.com>
2024-03-12 18:16:26 +0100

    removing unused method and improving delete method in FireStationDAO

--- src/main/java/net/safety/alerts/dao/FireStationsDAO.java
+++ src/main/java/net/safety/alerts/dao/FireStationsDAO.java
@@ -1,10 +1,8 @@
 package net.safety.alerts.dao;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import net.safety.alerts.model.AlertsData;
 import net.safety.alerts.model.FireStation;
 import org.springframework.stereotype.Repository;
-
 
 import java.util.List;
 
@@ -14,14 +12,6 @@ public class FireStationsDAO extends AlertsDAO{
     List<FireStation>fireStationsInFile = getData().getFireStations();
     List<FireStation>fireStationsInMemory = fireStationsInFile;
 
-    public FireStation findByAddress(String address) throws JsonProcessingException {
-        for(FireStation station : fireStationsInMemory){
-            if(station.getAddress().equalsIgnoreCase(address)){
-                return station;
-            }
-        }
-        return null;
-    }
 
     public List<FireStation>findAll(){
         return fireStationsInMemory;
@@ -32,8 +22,6 @@ public class FireStationsDAO extends AlertsDAO{
     }
 
     public void delete(String numberOrAddress) throws JsonProcessingException {
-        fireStationsInMemory.removeIf(fireStation -> {
-            return fireStation.getStation().equals(numberOrAddress)|| fireStation.getAddress().equalsIgnoreCase(numberOrAddress);
-        });
+        fireStationsInMemory.removeIf(fireStation -> fireStation.getStation().equals(numberOrAddress)|| fireStation.getAddress().equalsIgnoreCase(numberOrAddress));
     }
 }
